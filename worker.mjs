63a4dc37833b1c91bc2424bc384c3892dcae880e
Glen Maddern <glenmaddern@gmail.com>
2021-11-10 14:18:52 +0000

    Update worker.mjs

--- worker.mjs
+++ worker.mjs
@@ -6,12 +6,28 @@ export default {
     for (const [k, v] of request.headers.entries()) {
       headers[k] = v
     }
+    
+    let count = 'No DO attached :('
+    if (env.MY_DURABLE_OBJECT) {
+      let id = env.MY_DURABLE_OBJECT.idFromName("A");
+      let obj = env.MY_DURABLE_OBJECT.get(id);
+      let resp = await obj.fetch(request.url);
+      count = await resp.text();
+    }
+
+    let kv_response = 'nup'
+    if (env.MY_KV_NAMESPACE) {
+  	  kv_response = await env.MY_KV_NAMESPACE.get('OMFG')
+    }
+
 
     const payload = {
+      envs: Object.keys(env),
+      do_response: "Durable Object 'A' count: " + count,
+      kv_response,
       change: "HELLO NEW CONSTANT SCRIPT ID",
       headers,
       url,
-      envs: Object.keys(env),
       method,
       body: await request.text(),
       cf: request.cf
